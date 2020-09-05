e0acc97a8072ad0cb5bf6bb1b6f903849dea32c7
Matvey <matvey.w2015@mail.ru>
2020-09-05 23:21:17 +0400

    fix scrollbar && add video sidebar and footer && add ability to play and stop video

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './Video.css'
+import VideoFooter from '../VideoFooter/VideoFooter'
+import VideoSidebar from '../VideoSidebar/VideoSidebar'
 
 function Video() {
+    const [playing, setPlaying] = useState(false)
+    const videoRef = useRef(null)
+
+    const handeVideoPress = () => {
+        if (playing) {
+            videoRef.current.pause()
+            setPlaying(false)
+        } else {
+            videoRef.current.play()
+            setPlaying(true)
+        }
+    }
+
     return (
-        <div className='video'>
-            <video className='video__player' loop src="https://v16m.tiktokcdn.com/9b2398ce919cd89abfd16c5a0ae5fe67/5f542c84/video/tos/alisg/tos-alisg-pve-0037/75980d1b4dc64bcfb307aa86db738798/?a=1233&amp;br=1376&amp;bt=688&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=0&amp;ds=3&amp;er=&amp;l=20200905182500010189195071572FA9DC&amp;lr=tiktok_m&amp;mime_type=video_mp4&amp;qs=0&amp;rc=M3k5dzQ6Zmo8dzMzZDczM0ApNDZnODc2Mzw3Nzg2Zzc4aGdrcnEzbzUyZjJfLS0tMTRzc14xMC5eMF4xMzYvMDEvMDU6Yw%3D%3D&amp;vl=&amp;vr="></video>
+      <div className="video">
+        <video
+        onClick={handeVideoPress}
+          className="video__player"
+          ref={videoRef}
+          loop
+          src="https://v16m.tiktokcdn.com/9b2398ce919cd89abfd16c5a0ae5fe67/5f542c84/video/tos/alisg/tos-alisg-pve-0037/75980d1b4dc64bcfb307aa86db738798/?a=1233&amp;br=1376&amp;bt=688&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=0&amp;ds=3&amp;er=&amp;l=20200905182500010189195071572FA9DC&amp;lr=tiktok_m&amp;mime_type=video_mp4&amp;qs=0&amp;rc=M3k5dzQ6Zmo8dzMzZDczM0ApNDZnODc2Mzw3Nzg2Zzc4aGdrcnEzbzUyZjJfLS0tMTRzc14xMC5eMF4xMzYvMDEvMDU6Yw%3D%3D&amp;vl=&amp;vr="></video>
 
-            {/* VideoFooter */}
-            {/* VideoSideBar */}
-
-        </div>
-    )
+        {/* VideoFooter */}
+        <VideoFooter/>
+        {/* VideoSideBar */}
+        <VideoSidebar/>
+      </div>
+    );
 }
 
 export default Video
---
+++ src/components/VideoFooter/VideoFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './VideoFooter.css'
+
+function VideoFooter() {
+    return (
+        <div className='video__footer'>
+            
+        </div>
+    )
+}
+
+export default VideoFooter
