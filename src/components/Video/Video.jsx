a8c7023d65c1e36b541df254b9e5c24f3bb9cd7a
Matvey <matvey.w2015@mail.ru>
2020-09-05 22:48:20 +0400

    add video component and snap scrolling

---
+++ src/components/Video/Video.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Video.css'
+
+function Video() {
+    return (
+        <div className='video'>
+            <video className='video__player' loop src="https://v16m.tiktokcdn.com/9b2398ce919cd89abfd16c5a0ae5fe67/5f542c84/video/tos/alisg/tos-alisg-pve-0037/75980d1b4dc64bcfb307aa86db738798/?a=1233&amp;br=1376&amp;bt=688&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=0&amp;ds=3&amp;er=&amp;l=20200905182500010189195071572FA9DC&amp;lr=tiktok_m&amp;mime_type=video_mp4&amp;qs=0&amp;rc=M3k5dzQ6Zmo8dzMzZDczM0ApNDZnODc2Mzw3Nzg2Zzc4aGdrcnEzbzUyZjJfLS0tMTRzc14xMC5eMF4xMzYvMDEvMDU6Yw%3D%3D&amp;vl=&amp;vr="></video>
+
+            {/* VideoFooter */}
+            {/* VideoSideBar */}
+
+        </div>
+    )
+}
+
+export default Video
